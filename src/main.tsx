0938e10ab258f8828aaffff6613402ddc06bab1c
Richard Gonzalez <https://github.com/rich10anderson-ops/riccie-pallets-app.git>
2026-05-20 21:42:36 -0500

    fix: eliminar el componente AppProviders de main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,13 +2,9 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
-import { AppProviders } from "./providers/AppProviders.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <AppProviders>
-      <App />
-    </AppProviders>
+    <App />
   </StrictMode>,
 );
-
