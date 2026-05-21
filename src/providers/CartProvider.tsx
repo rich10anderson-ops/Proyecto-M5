fd6b6dc2bef60b9610c7620d82f43f865e59dd74
Richard Gonzalez <https://github.com/rich10anderson-ops/riccie-pallets-app.git>
2026-05-20 21:38:31 -0500

    fix: simplificar la exportación de CartProvider desde CartContext

--- src/providers/CartProvider.tsx
+++ src/providers/CartProvider.tsx
@@ -1,9 +1,1 @@
-import { CartContext } from "../contexts/CartContext";
-
-interface Props {
-  children: React.ReactNode;
-}
-
-export const CartProvider = ({ children }: Props) => {
-  return <CartContext.Provider value={{}}>{children}</CartContext.Provider>;
-};
+export { CartProvider } from "../contexts/CartContext";
